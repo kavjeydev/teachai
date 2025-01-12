ddd576ec0735ed4c31f4b4635a8e99d08a6a1bf0
kavjeydev <kjey11205@gmail.com>
2025-01-12 09:56:16 -0500

    auth config

---
+++ frontend/convex/auth.config.js
@@ -0,0 +1,10 @@
+export default {
+    providers: [
+      {
+        // domain: "https://hardy-firefly-8.clerk.accounts.dev",
+        domain: "https://clerk.trainlyai.com",
+        // domain: "https://descriptive-mallard-599.convex.cloud",
+        applicationID: "convex",
+      },
+    ],
+  };
