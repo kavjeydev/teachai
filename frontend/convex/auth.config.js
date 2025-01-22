d313cc61042ac91ba80a9ee780ebfd1bca68a436
kavjeydev <kjey11205@gmail.com>
2025-01-22 17:54:17 -0500

    enable clerk

--- frontend/convex/auth.config.js
+++ frontend/convex/auth.config.js
@@ -1,8 +1,8 @@
 export default {
   providers: [
     {
-      domain: "https://hardy-firefly-8.clerk.accounts.dev",
-      //   domain: "https://clerk.trainlyai.com",
+      // domain: "https://hardy-firefly-8.clerk.accounts.dev",
+      domain: "https://clerk.trainlyai.com",
       // domain: "https://descriptive-mallard-599.convex.cloud",
       applicationID: "convex",
     },
