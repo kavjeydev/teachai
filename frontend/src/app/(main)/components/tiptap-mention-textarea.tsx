19249b3423cfea4fdced8f8c92dab48b9a21d7df
kavjeydev <kjey11205@gmail.com>
2025-01-23 19:14:21 -0500

    files shown in @ command and not random things

--- frontend/src/app/(main)/components/tiptap-mention-textarea.tsx
+++ frontend/src/app/(main)/components/tiptap-mention-textarea.tsx
@@ -1,4 +1,5 @@
 import { IconBrandPython, IconBrandReact } from "@tabler/icons-react";
+import { useQuery } from "convex/react";
 
 import React, {
   forwardRef,
@@ -6,20 +7,26 @@ import React, {
   useImperativeHandle,
   useState,
 } from "react";
+import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
 
 interface MentionListProps {
-  items: string[];
+  items: { filename: string; fileId: string }[];
   command: (item: { id: string }) => void;
 }
 
 export const MentionList = forwardRef((props: MentionListProps, ref) => {
+  const chatId = window.location.href.split("/").slice(-1)[0] as Id<"chats">;
   const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const currentContext = useQuery(api.chats.getContext, { id: chatId });
+  props.items = currentContext || [];
 
   const selectItem = (index: any) => {
     const item = props.items[index];
 
     if (item) {
-      props.command({ id: item });
+      props.command({ id: item.filename });
     }
   };
 
@@ -76,7 +83,7 @@ export const MentionList = forwardRef((props: MentionListProps, ref) => {
           >
             <div className="flex gap-2 items-center">
               <IconBrandPython stroke={1} className="h-4 w-4" />
-              {item}
+              {item.filename}
             </div>
           </button>
         ))
